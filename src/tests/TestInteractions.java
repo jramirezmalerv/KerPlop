73d8b40e49ece0a23c57e54f2814ab28003ca289
Joshua Ramirez <jramirezmalerv@gmail.com>
2023-09-19 14:36:32 -0600

    Added JUnit tests

---
+++ src/tests/TestInteractions.java
@@ -0,0 +1,23 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+public class TestInteractions {
+	
+	
+	
+	
+	
+	@Test
+	public void testTreasureChest() {
+		
+		
+	}
+	
+	
+	@Test
+	public void testPowerUp() {
+		
+	}
+
+}
---
+++ src/tests/TestMovingPieces.java
@@ -0,0 +1,53 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
+
+import gameEngine.Drawable;
+import gameEngine.GameEngine;
+import gameEngine.InteractionResult;
+import levelPieces.Goblin;
+
+public class TestMovingPieces {
+	
+	
+	
+	
+	
+	
+	
+	// Test that Goblin hits player (movement) only when player is on the same space
+	
+	@Test
+	public void testGoblinMovement() {
+		Drawable [] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Goblin goblin = new Goblin(15);
+		gameBoard[15] = goblin;
+		
+		
+		assertEquals(InteractionResult.HIT, goblin.interact(gameBoard, 15));
+		
+		for(int i = 0; i < 14; i++) {
+			assertEquals(InteractionResult.NONE, goblin.interact(gameBoard, i));
+		}
+		
+		for(int i = 16; i < GameEngine.BOARD_SIZE; i++) {
+			assertEquals(InteractionResult.NONE, goblin.interact(gameBoard, i));
+		}
+		
+		
+		
+		
+
+	}
+	
+	
+	@Test 
+	public void testAlienMovement() {
+		fail("Not developed yet");
+	}
+}
+
+
+
