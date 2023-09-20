6884968ec6c881a4aad05dff65522f17d56d9c52
Joshua Ramirez <jramirezmalerv@gmail.com>
2023-09-19 19:35:43 -0600

    Ran testGoblinMovement() function: Passed

--- src/tests/TestMovingPieces.java
+++ src/tests/TestMovingPieces.java
@@ -7,15 +7,11 @@ import org.junit.jupiter.api.Test;
 import gameEngine.Drawable;
 import gameEngine.GameEngine;
 import gameEngine.InteractionResult;
+import levelPieces.Alien;
 import levelPieces.Goblin;
 
 public class TestMovingPieces {
-	
-	
-	
-	
-	
-	
+		
 	
 	// Test that Goblin hits player (movement) only when player is on the same space
 	
@@ -43,10 +39,22 @@ public class TestMovingPieces {
 	}
 	
 	
-	@Test 
-	public void testAlienMovement() {
-		fail("Not developed yet");
-	}
+//	@Test 
+//	public void testAlienMovement() {
+//		Drawable [] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+//		Alien alien = new Alien(5);
+//		gameBoard[5] = alien;
+//		
+//		assertEquals(InteractionResult.HIT, alien.interact(gameBoard, 15));
+//		
+//		for(int i = 0; i < 14; i++) {
+//			assertEquals(InteractionResult.NONE, alien.interact(gameBoard, i));
+//		}
+//		
+//		for(int i = 16; i < GameEngine.BOARD_SIZE; i++) {
+//			assertEquals(InteractionResult.NONE, alien.interact(gameBoard, i));
+//		}
+//	}
 }
 
 
