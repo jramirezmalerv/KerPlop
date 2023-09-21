3297a595b764bad2bfab511fb582f7b2f08104f4
NeutrinoBomb <graystamant@gmail.com>
2023-09-21 14:41:01 -0600

    Junit Interaction tests

--- src/tests/TestInteractions.java
+++ src/tests/TestInteractions.java
@@ -9,25 +9,61 @@ import gameEngine.Drawable;
 import gameEngine.GameEngine;
 import gameEngine.InteractionResult;
 import levelPieces.Goblin;
+import levelPieces.PowerUp;
+import levelPieces.TreasureChest;
+import levelPieces.Chair;
+import levelPieces.Alien;
+
+
 
 public class TestInteractions {
 
+	@Test
+	public void testDoor() {
+		Drawable [] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Chair chair = new Chair(15);
+		gameBoard[15] = chair;
+		
+		assertEquals(InteractionResult.NONE, chair.interact(gameBoard, 15));
 
-
-
+		for(int i = 0; i < GameEngine.BOARD_SIZE; i++) {
+			assertEquals(InteractionResult.NONE, chair.interact(gameBoard, i));
+		}
+	}
 
 	@Test
 	public void testTreasureChest() {
-		fail("Not yet implemented");
+		Drawable [] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		TreasureChest treasure = new TreasureChest(15);
+		gameBoard[15] = treasure;
+		
+		assertEquals(InteractionResult.ADVANCE, treasure.interact(gameBoard, 15));
 
+		for(int i = 0; i < 14; i++) {
+			assertEquals(InteractionResult.NONE, treasure.interact(gameBoard, i));
+		}
 
+		for(int i = 16; i < GameEngine.BOARD_SIZE; i++) {
+			assertEquals(InteractionResult.NONE, treasure.interact(gameBoard, i));
+		}
 	}
 
 
 	@Test
 	public void testPowerUp() {
-		fail("Not yet implemented");
+		Drawable [] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		PowerUp pu = new PowerUp(15);
+		gameBoard[15] = pu;
+		
+		assertEquals(InteractionResult.GET_POINT, pu.interact(gameBoard, 15));
+		
+		for(int i = 0; i < 14; i++) {
+			assertEquals(InteractionResult.NONE, pu.interact(gameBoard, i));
+		}
 
+		for(int i = 16; i < GameEngine.BOARD_SIZE; i++) {
+			assertEquals(InteractionResult.NONE, pu.interact(gameBoard, i));
+		}
 	}
 
 
@@ -48,6 +84,25 @@ public class TestInteractions {
 
 		for(int i = 16; i < GameEngine.BOARD_SIZE; i++) {
 			assertEquals(InteractionResult.NONE, goblin.interact(gameBoard, i));
+		}
+
+
+	}
+	
+	public void testAlien() {
+		Drawable [] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		Alien alien = new Alien(15);
+		gameBoard[15] = alien;
+
+
+		assertEquals(InteractionResult.HIT, alien.interact(gameBoard, 15));
+
+		for(int i = 0; i < 14; i++) {
+			assertEquals(InteractionResult.NONE, alien.interact(gameBoard, i));
+		}
+
+		for(int i = 16; i < GameEngine.BOARD_SIZE; i++) {
+			assertEquals(InteractionResult.NONE, alien.interact(gameBoard, i));
 		}
 
 
